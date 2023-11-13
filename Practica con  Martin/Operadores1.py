52ae15f35630ea0fe31638349811ab9e8609d15b
hugofmr <130927244+hugofmr@users.noreply.github.com>
2023-11-13 22:45:50 +0100

    hola

---
+++ Practica con  Martin/Operadores1.py
@@ -0,0 +1,29 @@
+a: int = 1
+b: int = 2
+'''
+suma: int = a + b
+
+print(f' Suma 1 + 2 = {suma} ')
+
+potencia: float = a ** b
+print(f'> Potencia 1 ** 2 = {potencia}')
+
+resto: float = a % b
+print(f"> Resto 1 % 2 = {resto}")
+
+operacion_compleja_2 = (33 * 10) + (2 / (5 ** 2))
+print(f"> Operación (33*10)+(2/(5**2)) = {operacion_compleja_2}")
+'''
+
+nombre: str = "Martín"
+apellidos: str = "San José de Vicente"
+  
+# Concatenación
+nombre_completo: str = nombre + " " + apellidos
+print(f"> Nombre completo: {nombre_completo}")
+  
+# Repetición
+nombre_x5: str = nombre*5
+print(f"> Nombre 5 veces: {nombre_x5}") 
+
+
---
+++ Practica con  Martin/Operadores2.py
@@ -0,0 +1,4 @@
+c: int = 3
+d: int = 3
+e: int = 4
+
