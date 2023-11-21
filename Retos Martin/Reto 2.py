0e66fa689bf74ee20115de8b5a0e08c45421abee
hugofmr <130927244+hugofmr@users.noreply.github.com>
2023-11-21 14:49:16 +0100

    ee
    aa

--- Retos Martin/Reto 2.py
+++ Retos Martin/Reto 2.py
@@ -3,12 +3,13 @@ Escribe un programa capaz de mostrar todos los números impares desde un número
 
 Por ejemplo: teniendo numero_inicial = 2 y numero_final = 8, el programa debe imprimir por consola: [3, 5, 7]
 '''
+# Inicio = int(input ('Indica numero inicial: '))
+numero_inicial: int = 2
 
-numero_inicial: int = 1
-numero_final: int = 22
+# fin= int('Indicame el numero final: ')
+numero_final: int = 8
 
-sumImpar= 0
-n1= numero_inicial + 1
-n2= numero_final - 1
-
+for i in range (numero_inicial, numero_final+1):
+    if i%2 != 0:
+        print(i)
 
---
+++ Retos Martin/Reto 3.py
@@ -0,0 +1,11 @@
+'''
+
+Escribe un programa que sea capaz de mostrar los números del 1 al 100 en orden inverso**.**
+
+'''
+
+numero_inicial= 1
+numero_final= 100
+
+for numero in range(numero_final, numero_inicial -1, -1):
+    print (numero)
